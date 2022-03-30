cac9e0afe65d3154f8c32e72acc0895c4926b44a
tiedaar <82518780+tiedaar@users.noreply.github.com>
2022-03-30 15:37:58 -0400

    Update building_wide_df.r

--- building_wide_df.r
+++ building_wide_df.r
@@ -1,7 +1,10 @@
 library(tidyr)
 library(dplyr)
 
+# Load the long dataframe, with zeros filled in for discourse elements with zero counts
 df <- read.csv("dfLong.csv")
+
+# Take out dependent variables besides discourse counts and remove duplicates
 dfPartial <- df %>%
   select(c(essay_id, holistic_score_adjudicated, prompt_name,
            gender, grade, ell, race_ethnicity,
@@ -9,10 +12,12 @@ dfPartial <- df %>%
            student_disability_status,task)) %>%
   distinct()
 
+# Make the discourse elements into wide form
 dfCounts <- df[1:4] %>%
   select(-c(X)) %>%
   spread(key = discourse_type, value = count) 
 
+# Merge the two dataframes
 dfWide <- merge(dfCounts, dfPartial, by="essay_id")
 write.csv(dfWide, "dfWide.csv")
 
